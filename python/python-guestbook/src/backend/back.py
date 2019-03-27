e88ca1d50dd5c48168fd85cefa22a06d954d7f8c
Dan Sanche <sanche@google.com>
2019-03-27 12:59:30 -0700

    removed debugger from code

--- python/python-guestbook/src/backend/back.py
+++ python/python-guestbook/src/backend/back.py
@@ -44,10 +44,6 @@ def add_message():
         abort(400)
 
 if __name__ == '__main__':
-    debug_port = os.getenv('DEBUG_PORT', None)
     server_port = os.getenv('PORT', 8080)
-
-    if debug_port is not None:
-        ptvsd.enable_attach(address=('0.0.0.0', debug_port))
 
     app.run(debug=False, port=server_port, host='0.0.0.0')
--- python/python-guestbook/src/frontend/front.py
+++ python/python-guestbook/src/frontend/front.py
@@ -35,10 +35,6 @@ def post():
     return redirect(url_for('main'))
 
 if __name__ == '__main__':
-    debug_port = os.getenv('DEBUG_PORT', None)
     server_port = os.getenv('PORT', 8080)
-
-    if debug_port is not None:
-        ptvsd.enable_attach(address=('0.0.0.0', debug_port))
 
     app.run(debug=False, port=server_port, host='0.0.0.0')

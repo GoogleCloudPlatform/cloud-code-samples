6f7ea06e07a44bbe1e24740a83c75a5c25e493d6
Dan Sanche <sanche@google.com>
2019-03-27 17:20:09 -0700

    removed unneeded imports

--- java/java-guestbook/frontend/src/main/java/cloudcode/guestbook/frontend/web/FrontendController.java
+++ java/java-guestbook/frontend/src/main/java/cloudcode/guestbook/frontend/web/FrontendController.java
@@ -1,16 +1,12 @@
 package cloudcode.guestbook.frontend.web;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.Map;
 import java.util.ArrayList;
 

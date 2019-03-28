a6b375dd060000b52c54edd3a409c2e1510c0729
Dan Sanche <sanche@google.com>
2019-03-27 17:18:29 -0700

    better error handling

--- java/java-guestbook/frontend/src/main/java/cloudcode/guestbook/frontend/web/FrontendController.java
+++ java/java-guestbook/frontend/src/main/java/cloudcode/guestbook/frontend/web/FrontendController.java
@@ -27,47 +27,34 @@ public class FrontendController {
     private String backendUri = String.format("http://%s/messages", System.getenv("GUESTBOOK_API_ADDR")); 
 
     @GetMapping("/")
-    public String main(Model model) {
-        try {
-            URL url = new URL(backendUri);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setReadTimeout(100);
+    public String main(Model model) throws IOException {
+        URL url = new URL(backendUri);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setReadTimeout(100);
 
-            InputStreamReader reader = new InputStreamReader(con.getInputStream(), "UTF-8");
-            ArrayList<Map<String, String>> messageList = new Gson().fromJson(reader, new TypeToken<ArrayList<Map<String, String>>>(){}.getType());
-            model.addAttribute("messages", messageList);
-            return "home"; //view
-        } catch (IOException e) {
-            return "error";
-        }
+        InputStreamReader reader = new InputStreamReader(con.getInputStream(), "UTF-8");
+        ArrayList<Map<String, String>> messageList = new Gson().fromJson(reader, new TypeToken<ArrayList<Map<String, String>>>(){}.getType());
+        model.addAttribute("messages", messageList);
+        return "home";
     }
 
     @RequestMapping(value="/post", method=RequestMethod.POST)
-    public String post(FormMessage formMessage) {
-        try {
-            URL url = new URL(backendUri);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestProperty( "Content-Type", "application/json" );
-            con.setRequestMethod("POST");
-            con.setDoOutput(true);
-            con.setReadTimeout(100);
-            con.setDoInput(true);
+    public String post(FormMessage formMessage) throws IOException {
+        URL url = new URL(backendUri);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty( "Content-Type", "application/json" );
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.setReadTimeout(100);
+        con.setDoInput(true);
 
-            OutputStream os = con.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");  
-            osw.write(formMessage.toString());
-            osw.flush();
-            if (con.getResponseCode() == HttpURLConnection.HTTP_OK){
-                System.out.print(con.getResponseMessage());
-            } else {
-                System.out.print(con.getResponseMessage());
-            }
-            osw.close(); 
-            return "redirect:/";
-        } catch (IOException e) {
-            return "error";
-        }
+        OutputStream os = con.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");  
+        osw.write(formMessage.toString());
+        osw.flush();
+        osw.close(); 
+        return "redirect:/";
     }
 
 }

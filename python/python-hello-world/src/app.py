772a3e9dad9eb2617873bcca8616e4599263a975
Dan Sanche <sanche@google.com>
2019-03-28 15:33:06 -0700

    removed DEBUG_PORT from hello world

--- python/python-hello-world/src/app.py
+++ python/python-hello-world/src/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
-import ptvsd
+try:
+    import ptvsd
+except:
+    pass
 
 # pylint: disable=C0103
 app = Flask(__name__)
@@ -12,7 +15,6 @@ def hello():
     return message
 
 if __name__ == '__main__':
-    debug_port = os.getenv('DEBUG_PORT', None)
     server_port = os.getenv('PORT', 8080)
 
     app.run(debug=False, port=server_port, host='0.0.0.0')

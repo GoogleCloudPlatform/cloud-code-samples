fd2d77df98238c7d6d7476f7ddbaa187c1f06b7c
Dan Sanche <sanche@google.com>
2019-03-28 23:11:29 -0700

    cleaned up validation code

--- java/java-guestbook/backend/src/main/java/cloudcode/guestbook/backend/web/BackendController.java
+++ java/java-guestbook/backend/src/main/java/cloudcode/guestbook/backend/web/BackendController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,31 +18,28 @@ import java.util.List;
 public class BackendController {
 
     @Autowired private MessageRepository repository;
+    static final List<String> validKeys = Arrays.asList("Author", "Message", "Date");
 
     @GetMapping("/messages")
     public List<Map<String, String>> getMessages() {
         List<Map<String, String>> msgList = repository.findAll();
-        return cleanList(msgList);
-    }
-
-    @PostMapping("/messages")
-    public void addMessage(@RequestBody Map<String, String> message) {
-        repository.save(cleanMap(message));
-    }
-
-    private  List<Map<String, String>> cleanList(List<Map<String, String>> inputList) {
         List<Map<String, String>> cleanedList = new ArrayList<Map<String, String>>();
-        for (Map<String, String> msg : inputList) {
-            cleanedList.add(cleanMap(msg));
+        for (Map<String, String> msg : msgList) {
+            cleanedList.add(cleanMessage(msg));
         }
         return cleanedList;
     }
 
-    private Map<String, String> cleanMap(Map<String, String> input){
+    @PostMapping("/messages")
+    public void addMessage(@RequestBody Map<String, String> message) {
+        repository.save(cleanMessage(message));
+    }
+
+    private Map<String, String> cleanMessage(Map<String, String> input){
         Map<String, String> cleaned = new HashMap<String, String>(input);
         ArrayList<String> keys = new ArrayList<>(input.keySet());
         for (String k : keys){
-            if (! k.equals("Author") && ! k.equals("Message") && !k.equals("Date")){
+            if (! validKeys.contains(k)){
                 cleaned.remove(k);
             }
         }

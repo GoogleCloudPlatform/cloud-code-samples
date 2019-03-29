762e591f9ff7b4b006690f47455ad9e4e010824c
Dan Sanche <sanche@google.com>
2019-03-29 13:24:23 -0700

    remove auth from mongo

--- java/java-guestbook/backend/src/main/java/cloudcode/guestbook/backend/web/MongodbDataSourceConfig.java
+++ java/java-guestbook/backend/src/main/java/cloudcode/guestbook/backend/web/MongodbDataSourceConfig.java
@@ -21,13 +21,7 @@ public class MongodbDataSourceConfig extends AbstractMongoConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        ServerAddress serverAddress = new ServerAddress(System.getenv("MONGO_HOST"));
-        List<MongoCredential> credentials = new ArrayList<>();
-        credentials.add(MongoCredential.createScramSha1Credential(
-                System.getenv("MONGO_USERNAME"),
-                "admin",
-                System.getenv("MONGO_PASSWORD").toCharArray()
-        ));
-        return new MongoClient(serverAddress, credentials);
+        ServerAddress serverAddress = new ServerAddress(System.getenv("GUESTBOOK_DB_ADDR"));
+        return new MongoClient(serverAddress);
     }
 }

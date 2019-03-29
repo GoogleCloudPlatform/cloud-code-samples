8d67f36bc91de6264be45de69e0d6c5fd2110bda
Guillermina Muro <guillermina.muro@gmail.com>
2019-03-29 13:30:46 -0700

    added current styles, serve public dir

--- nodejs/guestbook/src/frontend/app.js
+++ nodejs/guestbook/src/frontend/app.js
@@ -18,6 +18,8 @@ app.set("views", path.join(__dirname, "views"))
 const router = express.Router()
 
 app.use(router)
+
+app.use(express.static('public'))
 router.use(bodyParser.urlencoded({ extended: false }))
 
 // starts an http server on the $PORT environment variable

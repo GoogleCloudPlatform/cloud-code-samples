8efc767796da55801efe98f5dd605ba7d368915c
Dan Sanche <sanche@google.com>
2019-03-30 21:06:45 -0700

    simplified Python code. Limited validation

--- python/python-guestbook/src/backend/back.py
+++ python/python-guestbook/src/backend/back.py
@@ -6,31 +6,27 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from functools import reduce
 import bleach
+import datetime
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = 'mongodb://{}/guestbook'.format(os.environ.get('GUESTBOOK_DB_ADDR'))
 mongo = PyMongo(app)
-valid_keys = set(['Date', 'Author', 'Message'])
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
     """ retrieve and return the list of messages on GET request """
-    raw_data = list(mongo.db.messages.find())
-    cleaned_list = []
-    for msg in raw_data:
-        cleaned_msg = {k: bleach.clean(msg[k]) for k in msg if k in valid_keys}
-        cleaned_list.append(cleaned_msg)
-    return jsonify(cleaned_list)
+    msg_list = list(mongo.db.messages.find({}, {'author':1, 'message':1, 'date':1, '_id':0}).sort("_id", -1))
+    return jsonify(msg_list)
 
 @app.route('/messages', methods=['POST'])
 def add_message():
     """ save a new message on POST request """
     raw_data = request.get_json()
-    data = {k: bleach.clean(raw_data[k]) for k in raw_data if k in valid_keys}
-    if len(data) == len(valid_keys):
-        mongo.db.messages.insert_one(data)
-        return jsonify(message={'success':True}), 201
-    return jsonify(message={'success':False}), 400
+    msg_data = {'author':bleach.clean(raw_data['author']),
+                'message':bleach.clean(raw_data['message']),
+                'date':str(datetime.datetime.today())}
+    mongo.db.messages.insert_one(msg_data)
+    return  201
 
 if __name__ == '__main__':
     for v in ['PORT', 'GUESTBOOK_DB_ADDR']:
--- python/python-guestbook/src/frontend/front.py
+++ python/python-guestbook/src/frontend/front.py
@@ -3,7 +3,6 @@ A sample frontend server. Hosts a web page to display messages
 """
 import json
 import os
-import datetime
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 import requests
 import bleach
@@ -16,15 +15,13 @@ def main():
     """ Retrieve a list of messages from the backend, and use them to render the HTML template """
     response = requests.get(app.config["BACKEND_URI"], timeout=3)
     json_response = json.loads(response.text)
-    cleaned_list = [{k: bleach.clean(msg[k]) for k in msg} for msg in json_response]
-    return render_template('home.html', messages=cleaned_list)
+    return render_template('home.html', messages=json_response)
 
 @app.route('/post', methods=['POST'])
 def post():
     """ Send the new message to the backend and redirect to the homepage """
-    new_message = {'Author': request.form['name'],
-                   'Message':  request.form['message'],
-                   'Date': str(datetime.datetime.today())}
+    new_message = {'author': request.form['name'],
+                   'message':  request.form['message']}
     requests.post(url=app.config["BACKEND_URI"],
                   data=jsonify(new_message).data,
                   headers={'content-type': 'application/json'},

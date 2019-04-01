282cc0c624799e831450a24fc2e64992cb7f123c
Dan Sanche <sanche@google.com>
2019-04-01 00:58:59 -0700

    fixed backend return crash

--- python/python-guestbook/src/backend/back.py
+++ python/python-guestbook/src/backend/back.py
@@ -16,7 +16,7 @@ mongo = PyMongo(app)
 def get_messages():
     """ retrieve and return the list of messages on GET request """
     msg_list = list(mongo.db.messages.find({}, {'author':1, 'message':1, 'date':1, '_id':0}).sort("_id", -1))
-    return jsonify(msg_list)
+    return jsonify(msg_list), 201
 
 @app.route('/messages', methods=['POST'])
 def add_message():
@@ -26,7 +26,7 @@ def add_message():
                 'message':bleach.clean(raw_data['message']),
                 'date':datetime.datetime.today().strftime('%b %d, %-H:%M')}
     mongo.db.messages.insert_one(msg_data)
-    return  201
+    return  jsonify({}), 201
 
 if __name__ == '__main__':
     for v in ['PORT', 'GUESTBOOK_DB_ADDR']:

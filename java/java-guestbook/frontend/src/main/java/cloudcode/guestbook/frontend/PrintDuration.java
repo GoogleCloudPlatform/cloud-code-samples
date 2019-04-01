c0f589f9046b691a569f923fd1426019a77bdf80
Dan Sanche <sanche@google.com>
2019-04-01 00:21:24 -0700

    refactored print duration function

--- java/java-guestbook/frontend/src/main/java/cloudcode/guestbook/frontend/PrintDuration.java
+++ java/java-guestbook/frontend/src/main/java/cloudcode/guestbook/frontend/PrintDuration.java
@@ -15,22 +15,23 @@ public interface PrintDuration {
     LocalDateTime prev = LocalDateTime.ofInstant(Instant.ofEpochMilli(prevDateTimestamp), TimeZone.getDefault().toZoneId());
     Duration duration = Duration.between(now, prev);
 
-    if (Math.abs(duration.toDays()) >= 365*2) {
-      return String.format("%d years ago", Math.abs(duration.toDays())/365);
-    } else if (Math.abs(duration.toDays()) >= 365) {
-      return "1 year ago";
-    } else if (Math.abs(duration.toDays()) > 1) {
-      return String.format("%d days ago", Math.abs(duration.toDays()));
-    } else if (Math.abs(duration.toDays()) == 1) {
-      return "1 day ago";
-    } else if (Math.abs(duration.toHours()) > 1) {
-      return String.format("%d hours ago", Math.abs(duration.toHours()));
-    } else if (Math.abs(duration.toHours()) == 1) {
-      return "1 hour ago";
-    } else if (Math.abs(duration.toMinutes()) > 1){ 
-      return String.format("%d minutes ago", Math.abs(duration.toMinutes()));
+    long num;
+    String unit;
+    if (Math.abs(duration.toDays()) >= 365) {
+      num = Math.abs(duration.toDays())/365;
+      unit = num==1 ? "year" : "years";
+    } else if (Math.abs(duration.toDays()) > 0) {
+      num = Math.abs(duration.toDays());
+      unit = num==1 ? "day" : "days";
+    } else if (Math.abs(duration.toHours()) > 0){
+      num = Math.abs(duration.toHours());
+      unit = num==1 ? "hour" : "hours";  
+    } else if (Math.abs(duration.toMinutes()) > 0){ 
+      num = Math.abs(duration.toMinutes());
+      unit = num==1 ? "minute" : "minutes";
     } else {
       return "just now";
     }
+    return String.format("%d %s ago", num, unit);
   }
 }

2dbca7a61fbabf6db78c6e4e87e8bc36cf4968b8
David Gageot <david@gageot.net>
2019-04-02 13:27:18 +0200

    Don’t connect twice to MongoDB
    + The code in `messages.js` connects to the wrong url
    + The code in `app.js` should be moved to `messages.js`,
    where it’s used.
    
    Signed-off-by: David Gageot <david@gageot.net>

--- nodejs/nodejs-guestbook/src/backend/routes/messages.js
+++ nodejs/nodejs-guestbook/src/backend/routes/messages.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose')
-const MONGO_URI = process.env.DB_ADDRESS || 'mongodb://localhost:27017/test';
-mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root'
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'password'
+const MONGO_HOST = process.env.MONGO_HOST || 'mongo-service'
+const MONGO_PORT = process.env.MONGO_PORT || '27017'
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/admin`
+mongoose.connect(MONGO_URI, {useNewUrlParser: true})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection err:'));
+db.once('open', function() {
+  console.log('connected to ' + MONGO_URI);
+})
 
 const messageSchema = mongoose.Schema({
     name: { type: String, required: [true, 'Name is required'] },

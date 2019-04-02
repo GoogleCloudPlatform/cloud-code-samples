2211f1f1441a993ac2fe73c0d696ea67e30397cc
Guillermina Muro <guillermina.muro@gmail.com>
2019-04-02 14:28:52 -0700

    fails fast if env vars are not set

--- nodejs/nodejs-guestbook/src/backend/app.js
+++ nodejs/nodejs-guestbook/src/backend/app.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT
 const messages = require('./routes/messages')
 
 app.use('/', routes)
+
+if(!process.env.PORT) {
+  const errMsg = "PORT environment variable is not defined"
+  console.error(errMsg)
+  throw new Error(errMsg)
+}
+
+if(!process.env.GUESTBOOK_DB_ADDR) {
+  const errMsg = "GUESTBOOK_DB_ADDR environment variable is not defined"
+  console.error(errMsg)
+  throw new Error(errMsg)
+}
 
 // Connect to MongoDB, will retry only once
 messages.connectToMongoDB()

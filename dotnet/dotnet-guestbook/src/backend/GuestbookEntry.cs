729ef1ccb642a5949a8b9b5ebf7efaac4a4b5070
Simon Zeltser <zeltser@google.com>
2019-04-03 22:57:50 -0700

    Introducing guestbook sample for dotnet

---
+++ dotnet/dotnet-guestbook/src/backend/GuestbookEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend
+{
+    [BsonIgnoreExtraElements]
+    [DataContract]
+    public class GuestbookEntry
+    {
+        [DataMember(Name = "name")]
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "message")]
+        [BsonElement("Message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "date")]
+        [BsonElement("Date")]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ dotnet/dotnet-guestbook/src/frontend/Models/GuestbookEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace dotnet_guestbook.Models
+{
+    [DataContract]
+    public class GuestbookEntry
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "date")]
+        public DateTime Date { get; set; }
+    }
+}

0577fa11add1a4be3697ccc4faa88213124ccd28
Simon Zeltser <zeltser@google.com>
2019-06-05 01:56:09 +0000

    Fix trampoline linux format

--- .kokoro/trampoline.sh
+++ .kokoro/trampoline.sh
@@ -12,4 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 python3 "${KOKORO_GFILE_DIR}/trampoline_v1.py"

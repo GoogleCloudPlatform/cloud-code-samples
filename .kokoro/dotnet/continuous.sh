d316a79d50041d87e492fa2d8c3af9d98f8a999b
Simon Zeltser <zeltser@google.com>
2019-06-19 08:58:53 -0700

    adding skaffold to path

--- .kokoro/dotnet/continuous.sh
+++ .kokoro/dotnet/continuous.sh
@@ -11,6 +11,9 @@ export GCLOUD_PROJECT=cloud-code-samples-tests
 # Get skaffold
 curl -Lo skaffold https://storage.googleapis.com/skaffold/builds/latest/skaffold-linux-amd64
 chmod +x skaffold
+export PATH=$PATH:$(pwd)
+
+cd github/cloud-code-samples/dotnet
 
 # Building locally using skaffold
 skaffold build --default-repo gcr.io/${GCLOUD_PROJECT}

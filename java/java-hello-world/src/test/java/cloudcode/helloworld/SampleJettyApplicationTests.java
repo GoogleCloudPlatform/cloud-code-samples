30d90652059d803d4a1fe0b179e854522a45131a
Etan Shaul <eshaul@google.com>
2019-08-05 10:15:11 -0400

    undo unrelated chagnes

--- java/java-hello-world/src/test/java/cloudcode/helloworld/SampleJettyApplicationTests.java
+++ java/java-hello-world/src/test/java/cloudcode/helloworld/SampleJettyApplicationTests.java
@@ -2,28 +2,34 @@ package cloudcode.helloworld;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HelloWorldApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+@DirtiesContext
 public class SampleJettyApplicationTests {
 
-	@Autowired
-	private TestRestTemplate testRestTemplate;
-
+	@Value("${local.server.port}")
+	private int port;
 // TODO: review test
 	@Test
 	public void testHome() throws Exception {
-
-		ResponseEntity<String> responseEntity = this.testRestTemplate.getForEntity("/", String.class);
-		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-		assertEquals("Hello World!", responseEntity.getBody());
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + this.port, String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertEquals("Hello, world!", entity.getBody());
 	}
+
 }

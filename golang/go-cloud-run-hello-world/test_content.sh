22c25e90bfc55767e4fb73b997e3267abc0207f5
Adam Ross <adamross@google.com>
2020-02-19 15:41:22 -0800

    testing: simplify testing script, no reuse needed

--- golang/go-cloud-run-hello-world/test_content.sh
+++ golang/go-cloud-run-hello-world/test_content.sh
@@ -14,60 +14,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# retries, interval,keyword may be passed as arguments
-# retries: -r
-# interval (in seconds): -i
-# keyword: -k
-# url: -u
-
-## Defaults
-keyword="Chocolate Chip"
+PORT=${PORT:-8080}
+url=${1:-'http://localhost:$PORT'}
+expected='Congrats, you successfully deployed a container image to Cloud Run'
 retries=10
 interval=5
-url="http://localhost:3000"
-
-while getopts r:i:k:u: option
-do
-    case "${option}"
-    in
-        r) retries=$OPTARG;;
-        i) interval=$OPTARG;;
-        k) keyword=$OPTARG;;
-        u) url=$OPTARG;
-    esac
-done
-
-echo "START CONTENT TEST"
-echo "retries: "$retries
-echo "interval: "$interval
-echo "keyword: "$keyword
-echo "url: "$url
-
-if [[ -z "$url" ]]; then
-  echo "Failed: URL not specified."
-  exit 1
-fi
 
 for i in $(seq 0 $retries); do
     
     html="$(curl -si $url)" || html=""
     
-    if echo "$html" | grep -q "$keyword"
+    if echo "$html" | grep -q "$expected"
     then
-        echo "Keyword found -- site is up"
+        echo "Expected content found -- site is up"
         echo "END CONTENT TEST: Success! ✅"
         exit 0
     else
-        echo "keyword not found. retrying in $interval sec..."
+        echo "Expected content not found. retrying in $interval sec..."
         sleep $interval
     fi
 done
 
 echo "Error! Expected content not found."
-echo "Was looking for '$keyword'; not found in:"
+echo "Was looking for '$expected'; not found in:"
 echo "$html"
 echo "END CONTENT TEST: Fail! 💩"
-
-# If desired, cat the logs or copy to GCS
 
 exit 1

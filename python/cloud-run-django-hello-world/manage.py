5a9165532f1b1c70110fd441e50b90620a1ed305
Charles Engelke <engelke@google.com>
2020-03-16 17:07:45 -0700

    Use PORT environment variable

--- python/cloud-run-django-hello-world/manage.py
+++ python/cloud-run-django-hello-world/manage.py
@@ -8,6 +8,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloproject.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = os.environ.get('PORT', '8080')
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "

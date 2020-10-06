d91ac88755d810a16d22e366fe15df4d841c9a8f
kelsk <krippaehne@google.com>
2020-10-05 22:54:49 -0700

    Add index.html, update app.py

--- python/python-hello-world/src/app.py
+++ python/python-hello-world/src/app.py
@@ -1,22 +1,36 @@
+# Copyright 2020 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 A sample Hello World server.
 """
 import os
-from flask import Flask
+import requests
+
+from flask import Flask, render_template
 
 # pylint: disable=C0103
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    message = "Hello World"
-    return message
+    message = "It's running!"
+
+    return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    server_port = os.environ.get('PORT')
-    if server_port is None:
-        print("error: PORT environment variable not set")
-        exit(1)
-
+    server_port = os.environ.get('PORT', '8080')
     app.run(debug=False, port=server_port, host='0.0.0.0')

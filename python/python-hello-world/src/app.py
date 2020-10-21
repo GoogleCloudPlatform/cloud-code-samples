683bd1afb57deec47d9cc0efce1ff7f7e798839c
kelsk <krippaehne@google.com>
2020-10-21 13:21:17 -0700

    Update python-hello-world per reviews; update ci test

--- python/python-hello-world/src/app.py
+++ python/python-hello-world/src/app.py
@@ -1,22 +1,4 @@
-# Copyright 2020 Google LLC
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     https://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-A sample Hello World server.
-"""
 import os
-import requests
 
 from flask import Flask, render_template
 
@@ -26,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
+    """Return a simple HTML page with a friendly message."""
     message = "It's running!"
 
     return render_template('index.html', message=message)

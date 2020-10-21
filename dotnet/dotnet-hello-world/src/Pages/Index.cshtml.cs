df03d2c31038d3b9f66f5b6d30a8818a31c60482
kelsk <krippaehne@google.com>
2020-10-20 17:33:33 -0700

    Update dotnet-hello-world code per reviews

--- dotnet/dotnet-hello-world/src/Pages/Index.cshtml.cs
+++ dotnet/dotnet-hello-world/src/Pages/Index.cshtml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace src.Pages
+namespace dotnet_hello_world.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public string Message {get; private set; } = "It's running!";
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
     }
 }
--- dotnet/dotnet-hello-world/src/Startup.cs
+++ dotnet/dotnet-hello-world/src/Startup.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace src
+namespace dotnet_hello_world
 {
     public class Startup
     {

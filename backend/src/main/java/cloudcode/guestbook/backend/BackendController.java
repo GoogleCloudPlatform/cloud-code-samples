831c0ff4eb682ed3e9573ed23c0aa15e8a85ba96
Elijah Yap <yap@usc.edu>
2021-04-19 19:16:23 -0700

    add login endpoint

--- backend/src/main/java/cloudcode/guestbook/backend/BackendController.java
+++ backend/src/main/java/cloudcode/guestbook/backend/BackendController.java
@@ -1,14 +1,12 @@
 package cloudcode.guestbook.backend;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * defines the REST endpoints managed by the server.
@@ -16,32 +14,47 @@ import java.util.List;
 @RestController
 public class BackendController {
 
-    @Autowired private MessageRepository repository;
+  @Autowired
+  private MessageRepository repository;
 
-    /**
-     * endpoint for retrieving all guest book entries stored in database
-     * @return a list of GuestBookEntry objects
-     */
-    @GetMapping("/messages")
-    public final List<GuestBookEntry> getMessages() {
-        Sort byCreation = new Sort(Sort.Direction.DESC, "_id");
-        List<GuestBookEntry> msgList = repository.findAll(byCreation);
-        return msgList;
-    }
+  /**
+   * endpoint for retrieving all guest book entries stored in database
+   * @return a list of GuestBookEntry objects
+   */
+  @GetMapping("/messages")
+  public final List<GuestBookEntry> getMessages() {
+    Sort byCreation = new Sort(Sort.Direction.DESC, "_id");
+    List<GuestBookEntry> msgList = repository.findAll(byCreation);
+    return msgList;
+  }
 
-    /**
-     * endpoint for adding a new guest book entry to the database
-     * @param message a message object passed in the HTTP POST request
-     */
-    @PostMapping("/messages")
-    public final void addMessage(@RequestBody GuestBookEntry message) {
-        message.setDate(System.currentTimeMillis());
-        repository.save(message);
-    }
-    
-    @PostMapping("/signup")
-    public final void addUser(@RequestBody GuestBookEntry message) {
-        message.setDate(System.currentTimeMillis());
-        repository.save(message);
-    }
+  /**
+   * endpoint for retrieving all guest book entries stored in database
+   * @return a list of GuestBookEntry objects
+   */
+  @GetMapping("/login")
+  public final List<GuestBookEntry> login(@RequestBody GuestBookEntry message) {
+    message.setDate(System.currentTimeMillis());
+    List<GuestBookEntry> msgList = repository.findByUsernameAndPassword(
+      message.getUsername(),
+      message.getPassword()
+    );
+    return msgList;
+  }
+
+  /**
+   * endpoint for adding a new guest book entry to the database
+   * @param message a message object passed in the HTTP POST request
+   */
+  @PostMapping("/messages")
+  public final void addMessage(@RequestBody GuestBookEntry message) {
+    message.setDate(System.currentTimeMillis());
+    repository.save(message);
+  }
+
+  @PostMapping("/signup")
+  public final void addUser(@RequestBody GuestBookEntry message) {
+    message.setDate(System.currentTimeMillis());
+    repository.save(message);
+  }
 }

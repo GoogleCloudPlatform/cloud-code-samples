46a3a7ab0d8099253828c4077ec7e2ce9c02806f
Elijah Yap <yap@usc.edu>
2021-04-19 19:21:18 -0700

    Update MessageRepository.java

--- backend/src/main/java/cloudcode/guestbook/backend/MessageRepository.java
+++ backend/src/main/java/cloudcode/guestbook/backend/MessageRepository.java
@@ -1,9 +1,15 @@
 package cloudcode.guestbook.backend;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * represents a custom Mongo repository that stores GuestBookEntry objects
  */
-public interface MessageRepository extends
-    MongoRepository<GuestBookEntry, String> { }
+public interface MessageRepository
+  extends MongoRepository<GuestBookEntry, String> {
+  List<GuestBookEntry> findByUsernameAndPassword(
+    String username,
+    String password
+  );
+}

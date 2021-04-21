eaac8906973ddc9443d4373a323999ef7227a1ae
Elijah Yap <yap@usc.edu>
2021-04-21 06:18:59 -0700

    build basic user detail service

--- backend/src/main/java/cloudcode/guestbook/backend/CustomUserDetailsService.java
+++ backend/src/main/java/cloudcode/guestbook/backend/CustomUserDetailsService.java
@@ -1,7 +1,36 @@
 package cloudcode.guestbook.backend;
 
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {}
+public class CustomUserDetailsService implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  //   @Autowired
+  //   private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @Override
+  public UserDetails loadUserByUsername(String email)
+    throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email);
+    if (user != null) {
+      return new org.springframework.security.core.userdetails.User(
+        user.getEmail(),
+        user.getPassword(),
+        // pass empty list – no authorities
+        new ArrayList<GrantedAuthority>()
+      );
+    } else {
+      throw new UsernameNotFoundException("Username not found");
+    }
+  }
+}

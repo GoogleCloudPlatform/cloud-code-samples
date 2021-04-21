011fc071d73a10eb34652c34208ac35314b12e79
Elijah Yap <yap@usc.edu>
2021-04-21 06:12:29 -0700

    add annotations to user

--- backend/src/main/java/cloudcode/guestbook/backend/User.java
+++ backend/src/main/java/cloudcode/guestbook/backend/User.java
@@ -1,44 +1,60 @@
 package cloudcode.guestbook.backend;
 
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * defines the data associated with a single guest book entry
  */
+@Document(collection = "user")
 public class User {
 
-    private String email;
-    private String username;
-    private String password;
-    private long date;
+  @Indexed(
+    unique = true,
+    direction = IndexDirection.DESCENDING,
+    dropDups = true
+  )
+  private String email;
+  
+  @Indexed(
+    unique = true,
+    direction = IndexDirection.DESCENDING,
+    dropDups = true
+  )
+  private String username;
+  private String password;
+  private long date;
 
-    public final String getEmail() {
-        return email;
-    }
+  public final String getEmail() {
+    return email;
+  }
 
-    public final void setEmail(String email) {
-        this.email = email;
-    }
+  public final void setEmail(String email) {
+    this.email = email;
+  }
 
-    public final String getUsername() {
-        return username;
-    }
+  public final String getUsername() {
+    return username;
+  }
 
-    public final void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public final String getPassword() {
-        return password;
-    }
-    
-    public final void setPassword(String password) {
-        this.password = password;
-    }
+  public final void setUsername(String username) {
+    this.username = username;
+  }
 
-    public final long getDate() {
-        return this.date;
-    }
+  public final String getPassword() {
+    return password;
+  }
 
-    public final void setDate(long date) {
-        this.date = date;
-    }
+  public final void setPassword(String password) {
+    this.password = password;
+  }
+
+  public final long getDate() {
+    return this.date;
+  }
+
+  public final void setDate(long date) {
+    this.date = date;
+  }
 }

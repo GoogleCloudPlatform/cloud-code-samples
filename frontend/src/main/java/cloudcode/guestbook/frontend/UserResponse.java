1f492fc5f9fd2ff0f3101f0a7742f0e8c7320beb
Elijah Yap <yap@usc.edu>
2021-04-21 16:13:23 -0700

    add default constructor to avoid Jackson error

--- frontend/src/main/java/cloudcode/guestbook/frontend/UserResponse.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/UserResponse.java
@@ -5,6 +5,8 @@ public class UserResponse {
   public Boolean success;
   public String errorMessage;
 
+  public UserResponse() {}
+
   public UserResponse(boolean success, String errorMessage) {
     this.success = success;
     this.errorMessage = errorMessage;

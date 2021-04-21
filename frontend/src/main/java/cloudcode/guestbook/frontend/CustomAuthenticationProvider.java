126f4c5b0f45c78d4ab53801305fb6a5a05cefcd
Elijah Yap <yap@usc.edu>
2021-04-21 15:19:42 -0700

    sling test user object to backend when authenticating

--- frontend/src/main/java/cloudcode/guestbook/frontend/CustomAuthenticationProvider.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/CustomAuthenticationProvider.java
@@ -1,15 +1,24 @@
 package cloudcode.guestbook.frontend;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpEntity;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+  private String loginUri = String.format(
+    "http://%s/login",
+    System.getenv("GUESTBOOK_API_ADDR")
+  );
 
   private static List<User> users = new ArrayList<User>();
 
@@ -23,6 +32,17 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     throws AuthenticationException {
     String username = authentication.getPrincipal().toString();
     String password = authentication.getCredentials().toString();
+    try {
+      new RestTemplate()
+      .postForObject(
+          new URI(loginUri),
+          new HttpEntity<User>(new User("", username, password)),
+          SignupResponse.class
+        );
+    } catch (URISyntaxException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     //TODO: call backend
     return new UsernamePasswordAuthenticationToken(username, password);
   }

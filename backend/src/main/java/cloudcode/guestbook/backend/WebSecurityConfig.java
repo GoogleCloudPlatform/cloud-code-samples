b5d000124e9329ca8c81f0371a440344b04a09e3
Elijah Yap <yap@usc.edu>
2021-04-21 06:04:41 -0700

    change import and enable all requests

---
+++ backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package cloudcode.guestbook.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+  }
+
+  @Override
+  protected void configure(final AuthenticationManagerBuilder auth)
+    throws Exception {
+    auth
+      .inMemoryAuthentication()
+      .withUser("user1")
+      .password(passwordEncoder().encode("user1Pass"))
+      .roles("USER")
+      .and()
+      .withUser("user2")
+      .password(passwordEncoder().encode("user2Pass"))
+      .roles("USER")
+      .and()
+      .withUser("admin")
+      .password(passwordEncoder().encode("adminPass"))
+      .roles("ADMIN");
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}

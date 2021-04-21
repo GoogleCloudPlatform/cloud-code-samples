33793840f95884a19369974a0291014493600ea9
Elijah Yap <yap@usc.edu>
2021-04-21 12:39:01 -0700

    remove unused import

--- frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package cloudcode.guestbook.frontend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;

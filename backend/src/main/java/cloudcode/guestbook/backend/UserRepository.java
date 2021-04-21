9549cdac94c0fb7b7686a0e585c24de31ebb5fc4
Elijah Yap <yap@usc.edu>
2021-04-21 15:20:01 -0700

    change user repo to get unique value

--- backend/src/main/java/cloudcode/guestbook/backend/UserRepository.java
+++ backend/src/main/java/cloudcode/guestbook/backend/UserRepository.java
@@ -1,13 +1,12 @@
 package cloudcode.guestbook.backend;
 
-import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * represents a custom Mongo repository that stores User objects
  */
 public interface UserRepository extends MongoRepository<User, String> {
-  List<User> findByUsernameAndPassword(String username, String password);
+  User findByUsernameAndPassword(String username, String password);
   User findByEmail(String email);
   User findByUsername(String username);
 }

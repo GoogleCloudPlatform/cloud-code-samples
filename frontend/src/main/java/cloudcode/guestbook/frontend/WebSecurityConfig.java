b6d7c808d84b41ee65e1dc9236e125b6d4016a33
Elijah Yap <yap@usc.edu>
2021-04-21 13:10:19 -0700

    organize DSL

--- frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
@@ -15,20 +15,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .authorizeRequests()
-      .antMatchers("/", "/signup")
-      .permitAll()
-      .antMatchers("/css/**")
-      .permitAll()
-      .anyRequest()
-      .authenticated()
-      .and()
-      .formLogin()
-      .loginPage("/login")
-      .permitAll()
-      .and()
-      .logout()
-      .permitAll();
+        .authorizeRequests()
+            .antMatchers("/", "/signup", "/css/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login").permitAll()
+            .and()
+        .logout()
+            .permitAll();
   }
 
   @Bean

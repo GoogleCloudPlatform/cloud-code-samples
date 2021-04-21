4755b41aa9712b815aa617cacf19f6f94be7da33
Elijah Yap <yap@usc.edu>
2021-04-21 10:52:25 -0700

    replace in memory login system with custom user details syste,

--- backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
+++ backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
@@ -1,13 +1,14 @@
 package cloudcode.guestbook.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,23 +22,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(final AuthenticationManagerBuilder auth)
     throws Exception {
+    UserDetailsService userDetailsService = mongoUserDetails();
     auth
-      .inMemoryAuthentication()
-      .withUser("user1")
-      .password(passwordEncoder().encode("user1Pass"))
-      .roles("USER")
-      .and()
-      .withUser("user2")
-      .password(passwordEncoder().encode("user2Pass"))
-      .roles("USER")
-      .and()
-      .withUser("admin")
-      .password(passwordEncoder().encode("adminPass"))
-      .roles("ADMIN");
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(bCryptPasswordEncoder);
   }
 
+  @Autowired
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
   @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+  public UserDetailsService mongoUserDetails() {
+    return new CustomUserDetailsService();
   }
 }

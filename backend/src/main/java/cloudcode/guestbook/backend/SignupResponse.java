1952e40e12e1e0629ed7ba89e01efce66f16f8bc
Elijah Yap <yap@usc.edu>
2021-04-21 06:46:13 -0700

    add constructor

--- backend/src/main/java/cloudcode/guestbook/backend/SignupResponse.java
+++ backend/src/main/java/cloudcode/guestbook/backend/SignupResponse.java
@@ -3,4 +3,9 @@ package cloudcode.guestbook.backend;
 public class SignupResponse {
     public Boolean success;
     public String errorMessage;
+
+    public SignupResponse(boolean success, String errorMessage) {
+        this.success = success;
+        this.errorMessage = errorMessage;
+    }
 }

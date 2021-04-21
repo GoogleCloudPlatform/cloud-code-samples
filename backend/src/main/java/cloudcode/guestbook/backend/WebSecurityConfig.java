4a5d9103cdea1331ae11cc4c1fa16ebd31ca68ba
Elijah Yap <yap@usc.edu>
2021-04-21 15:54:39 -0700

    remove import and auto format

--- backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
+++ backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package cloudcode.guestbook.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
 // import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -23,13 +23,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(final AuthenticationManagerBuilder auth)
     throws Exception {
     UserDetailsService userDetailsService = mongoUserDetails();
-    auth
-      .userDetailsService(userDetailsService);
+    auth.userDetailsService(userDetailsService);
     //   .passwordEncoder(bCryptPasswordEncoder);
   }
 
-//   @Autowired
-//   private BCryptPasswordEncoder bCryptPasswordEncoder;
+  //   @Autowired
+  //   private BCryptPasswordEncoder bCryptPasswordEncoder;
 
   @Bean
   public UserDetailsService mongoUserDetails() {

8317a6f878a1615951e930438d28553e5fb20bf7
Elijah Yap <yap@usc.edu>
2021-04-21 10:26:18 -0700

    add method to find user by username

--- backend/src/main/java/cloudcode/guestbook/backend/UserRepository.java
+++ backend/src/main/java/cloudcode/guestbook/backend/UserRepository.java
@@ -9,4 +9,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface UserRepository extends MongoRepository<User, String> {
   List<User> findByUsernameAndPassword(String username, String password);
   User findByEmail(String email);
+  User findByUsername(String username);
 }

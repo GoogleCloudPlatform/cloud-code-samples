fb1875ba82fc45b05f0053270fba9f705fe5ca4b
Elijah Yap <yap@usc.edu>
2021-04-21 05:34:51 -0700

    make a test import from security

--- backend/src/main/java/cloudcode/guestbook/backend/CustomUserDetailsService.java
+++ backend/src/main/java/cloudcode/guestbook/backend/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package cloudcode.guestbook.backend;
 
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-// @Service
-// public class CustomUserDetailsService implements UserDetailsService {}
+@Service
+public class CustomUserDetailsService implements UserDetailsService {}

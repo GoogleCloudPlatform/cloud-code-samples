a64602cb1955696a3461fcee265b026991bfed7e
Elijah Yap <yap@usc.edu>
2021-04-21 13:20:53 -0700

    change auth provider to builder
    matches previous position of In Memory Auth Provider

--- frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package cloudcode.guestbook.frontend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .authenticationProvider(authenticationProvider())
         .authorizeRequests()
             .antMatchers("/", "/signup", "/css/**").permitAll()
             .anyRequest().authenticated()
@@ -28,6 +28,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll();
   }
 
+  @Override
+  protected void configure(final AuthenticationManagerBuilder auth) {
+      auth.authenticationProvider(authenticationProvider());
+  }
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();

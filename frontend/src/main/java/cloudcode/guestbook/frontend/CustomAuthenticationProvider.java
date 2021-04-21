1055eb7a46e811fcd24e78133115b7a210f04ab1
Elijah Yap <yap@usc.edu>
2021-04-21 13:15:07 -0700

    write test implementation of Auth method

--- frontend/src/main/java/cloudcode/guestbook/frontend/CustomAuthenticationProvider.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/CustomAuthenticationProvider.java
@@ -1,22 +1,24 @@
 package cloudcode.guestbook.frontend;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
-    @Override
-    public Authentication authenticate(Authentication authentication)
-            throws AuthenticationException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public Authentication authenticate(Authentication authentication)
+    throws AuthenticationException {
+    String username = authentication.getPrincipal().toString();
+    String password = authentication.getCredentials().toString();
+    //TODO: call backend
+    return new UsernamePasswordAuthenticationToken(username, password);
+  }
 
-    @Override
-    public boolean supports(Class<?> authentication) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-    
+  @Override
+  public boolean supports(Class<?> authentication) {
+    // TODO Auto-generated method stub
+    return false;
+  }
 }

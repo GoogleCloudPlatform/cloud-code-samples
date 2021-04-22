76a2b843d42774cf67928e8d3864284391a3ed59
Elijah Yap <yap@usc.edu>
2021-04-22 10:12:43 -0700

    allow certain methods to POST
    https://stackoverflow.com/questions/48476279/spring-websecurityconfigureradapter-allow-post

--- frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package cloudcode.guestbook.frontend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +29,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
         .logout()
             .permitAll();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring()
+      .antMatchers(HttpMethod.POST, "/signup")
+      .antMatchers(HttpMethod.POST, "/tokensignin");
   }
 
   @Override

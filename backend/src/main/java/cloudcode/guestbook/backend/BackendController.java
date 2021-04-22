29dabbcc0b452f0231192baef3dbf64eb1686980
Elijah Yap <yap@usc.edu>
2021-04-22 05:59:50 -0700

    remove unused imports

--- backend/src/main/java/cloudcode/guestbook/backend/BackendController.java
+++ backend/src/main/java/cloudcode/guestbook/backend/BackendController.java
@@ -3,14 +3,9 @@ package cloudcode.guestbook.backend;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.authentication.AccountStatusException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**

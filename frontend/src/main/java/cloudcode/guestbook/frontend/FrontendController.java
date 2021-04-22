4f8999d8bd6bea3efe1989a057254244ca342600
Elijah Yap <yap@usc.edu>
2021-04-22 05:36:39 -0700

    condition signup form redirect on response success

--- frontend/src/main/java/cloudcode/guestbook/frontend/FrontendController.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/FrontendController.java
@@ -95,7 +95,10 @@ public class FrontendController {
       httpEntity,
       UserResponse.class
     );
-
-    return "redirect:/";
+    if (response.success) {
+      return "redirect:/";
+    } else {
+      return "redirect:/login?error=" + response.errorMessage;
+    }
   }
 }

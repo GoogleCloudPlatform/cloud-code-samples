ec7c4b623e447d37a3092c597453e0a17b101de8
Elijah Yap <yap@usc.edu>
2021-04-22 10:13:37 -0700

    remove post endpoints from main thing

--- frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-            .antMatchers("/", "/signup", "/tokensignin", "/css/**").permitAll()
+            .antMatchers("/", "/css/**").permitAll()
             .anyRequest().authenticated()
             .and()
         .formLogin()

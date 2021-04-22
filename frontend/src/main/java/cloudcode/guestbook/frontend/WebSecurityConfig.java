1830399313a2d089834abfd80ed562a4752c5d8b
Elijah Yap <yap@usc.edu>
2021-04-22 11:19:49 -0700

    add failure endpoint

--- frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
         .formLogin()
             .loginPage("/login").permitAll()
+            .failureUrl("/login-error")
             .and()
         .logout()
             .permitAll();

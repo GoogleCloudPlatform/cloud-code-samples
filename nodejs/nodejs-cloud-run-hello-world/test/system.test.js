316b7e624bbcc232e515d8ab4eab86167553e9aa
kelsk <krippaehne@google.com>
2021-07-15 11:36:44 -0700

    Add authorization to Node.js test

--- nodejs/nodejs-cloud-run-hello-world/test/system.test.js
+++ nodejs/nodejs-cloud-run-hello-world/test/system.test.js
@@ -3,12 +3,18 @@ const {request} = require('gaxios');
 
 const port = process.env.PORT || '8080';
 const url = process.env.SERVICE_URL || `http://localhost:${port}`;
+const token = process.env.TOKEN || '';
 
 describe('Hello World', () => {
   it('can respond to an HTTP request', async () => {
-    console.log(`    - Requesting GET ${url}/...`)
+    console.log(`    - Requesting GET ${url}/...`);
+
+    const header = {};
+    if (token) header['Authorization'] = 'Bearer ' + token;
+
     const res = await request({
       url: url + '/',
+      headers: header,
       timeout: 5000,
     });
 

c191661a3b09dcad34e8657aea400a432a11c936
davidponder <davidponder@google.com>
2022-10-26 17:03:34 -0700

    Remove unnecessary readmes and fix nodejs sample

--- nodejs/cloud-function-hello-world/index.js
+++ nodejs/cloud-function-hello-world/index.js
@@ -2,6 +2,6 @@ const functions = require('@google-cloud/functions-framework');
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
-functions.http('helloGET', (req, res) => {
+functions.http('helloHttp', (req, res) => {
   res.send('Hello World!');
 });

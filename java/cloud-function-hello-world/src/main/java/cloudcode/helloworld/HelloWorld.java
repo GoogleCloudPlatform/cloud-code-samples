983deaeddaf5f3fa34d8c36e7182d713082a8530
davidponder <davidponder@google.com>
2022-10-27 09:06:08 -0700

    Fix Java sample

--- java/cloud-function-hello-world/src/main/java/cloudcode/helloworld/HelloWorld.java
+++ java/cloud-function-hello-world/src/main/java/cloudcode/helloworld/HelloWorld.java
@@ -1,16 +1,13 @@
-package functions;
+package cloudcode.helloworld;
 
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import java.io.BufferedWriter;
-import java.io.IOException;
 
 public class HelloWorld implements HttpFunction {
-  // Simple function to return "Hello World"
   @Override
-  public void service(HttpRequest request, HttpResponse response)
-      throws IOException {
+  public void service(HttpRequest request, HttpResponse response) throws Exception {
     BufferedWriter writer = response.getWriter();
     writer.write("Hello World!");
   }

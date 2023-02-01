7c608c9e76d2f7c489d62aa823334c475a30dd4d
GustavoSTZ <stzgustavo@gmail.com>
2023-02-01 19:28:48 -0300

    remove unnecessary imports

--- java/java-cloud-run-hello-world/src/test/java/cloudcode/helloworld/web/HelloWorldControllerIT.java
+++ java/java-cloud-run-hello-world/src/test/java/cloudcode/helloworld/web/HelloWorldControllerIT.java
@@ -5,15 +5,12 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 
 import java.io.IOException;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;

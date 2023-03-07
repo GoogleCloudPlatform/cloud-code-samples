5bffa8b7aa0fdccdb7567a3d78d96105d8b1fc0e
Damon Douglas <damondouglas@google.com>
2023-03-07 23:48:36 +0000

    Instructional code comments for JsonToRow

--- java/java-dataflow-samples/read-pubsub-write-bigquery/src/main/java/com/cloudcode/dataflow/example/conversions/JsonToRow.java
+++ java/java-dataflow-samples/read-pubsub-write-bigquery/src/main/java/com/cloudcode/dataflow/example/conversions/JsonToRow.java
@@ -21,6 +21,12 @@ import org.apache.beam.sdk.values.TypeDescriptor;
  * {@link JsonToRow} is a {@link PTransform} that converts JSON encoded strings into a {@link
  * PCollectionTuple} containing the {@link PCollection} of converted {@link Row}s and the {@link
  * PCollection} of errors.
+ *
+ * <p>This example shows how to use an additional outputs pattern to output multiple PCollections.
+ * We do this when we want to output our expected PCollection from successful processing as well as
+ * an error PCollection to collect any errors. The value of this design pattern is that we can allow
+ * the pipeline to continue normal processing while sending errors to further processing, storage or
+ * logs. See: https://beam.apache.org/documentation/programming-guide/#additional-outputs.
  */
 public class JsonToRow<UserT> extends PTransform<PCollection<String>, PCollectionTuple> {
 
@@ -52,11 +58,21 @@ public class JsonToRow<UserT> extends PTransform<PCollection<String>, PCollectio
 
   @Override
   public PCollectionTuple expand(PCollection<String> input) {
+    // We implement this PTransform using two steps.
+    // The first step parses the PCollection of JSON encoded strings to Beam Rows.
     Result<PCollection<Row>, String> result = input.apply("To Row", jsonToRowWithFailures());
+
+    // The second step bundles the expected output and any errors into a PCollectionTuple.
+    // See https://beam.apache.org/documentation/programming-guide/#additional-outputs
+    // for more information about a PCollectionTuple.
     return PCollectionTuple.of(ROW, result.output().setRowSchema(payloadSerializer.getSchema()))
         .and(ERROR, result.failures());
   }
 
+  /**
+   * Instead of using the typical {@link MapElements} to map from a JSON String to a Beam {@link
+   * Row}, we use {@link MapElements.MapWithFailures} to collect any possible errors.
+   */
   private MapElements.MapWithFailures<String, Row, String> jsonToRowWithFailures() {
     return MapElements.into(rows())
         .via((String json) -> payloadSerializer.deserialize(requireNonNull(json)))

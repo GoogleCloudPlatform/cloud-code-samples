1ae79244b55a9f6e71eea05ae4e301af686a404a
Averi Kitsch <akitsch@google.com>
2023-08-28 14:19:44 -0700

    fix: add license headers - dotnet

--- dotnet/dotnet-guestbook/src/frontend/wwwroot/js/site.js
+++ dotnet/dotnet-guestbook/src/frontend/wwwroot/js/site.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 

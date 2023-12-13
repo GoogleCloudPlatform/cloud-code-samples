c78ba7a205fd1f1cc83f033eb3b1cdace822d773
Jay Windsor <jaywindsor@google.com>
2023-12-13 11:47:55 -0500

    PR comments

--- python/gemini-api/main.py
+++ python/gemini-api/main.py
@@ -1,14 +1,20 @@
+import os
+
 import google.generativeai as genai
 
-# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
-GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+# Using `GOOGLE_API_KEY` environment variable.
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
+# Checking for all available models with generateContent feature
+for model in genai.list_models():
+    if "generateContent" in model.supported_generation_methods:
+        print(model.name)
 
+# Initiate the Model
 model = genai.GenerativeModel(model_name="gemini-pro")
 
+# Query the Model
 response = model.generate_content("How do I bake a cake?")
+
 print(response.text)
